0c9acd91da5b7ca985c0490592caf9e8292d228b
Krystal Jean Austin <44277065+KrystalJean@users.noreply.github.com>
2022-10-26 13:17:33 -0500

    made min/sec two digits, fixed am/pm bug, fixes needle rotation

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ const toggle = document.querySelector('.toggle');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+//Function for Theme Changing Button
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html');
-    if(html.classList.contains('dark')) {
+    if (html.classList.contains('dark')) {
         html.classList.remove('dark');
         e.target.innerHTML = 'Dark Mode';
     } else {
@@ -21,22 +22,32 @@ toggle.addEventListener('click', (e) => {
 
 function setTime() {
     const time = new Date();
+    const year = time.getFullYear();
     const month = time.getMonth();
     const day = time.getDay();
+    const date = time.getDate();
     const hours = time.getHours();
-    const hoursForClock = hours % 12;
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
+    let hoursForClock = hours;
+    //This if statement is for standard time, and can be commented out for military time. 
+    if (hours > 12) {
+        hoursForClock = hours - 12;
+    }
+    const minutes = String(time.getMinutes()).padStart(2, '0');
+    const seconds = String(time.getSeconds()).padStart(2, '0');
+    const ampm = hours > 11 || hours < 24 ? 'PM' : 'AM';
+
+    //Utility Function: maps one range of numbers to another range of numbers.
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+        return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
 
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
 
-    timeEl.innerHTML = `${hours }:${minutes}`;
-}
+    timeEl.innerHTML = `${hoursForClock}:${minutes}:${seconds} ${ampm}`;
+    dateEl.innerHTML = `~ ${days[day]}, ${months[month]} <span class="circle">${date}</span> ${year} ~`;
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 setTime();
